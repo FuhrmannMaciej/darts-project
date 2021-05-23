cef3ca1cf10259168609911f4d60830089a5c973
Maciej Fuhrmann <fuhrmann.maciej@gmail.com>
2021-05-23 16:59:27 +0200

    Fixes for responsivness, added smooth scroll behaviour

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ document.querySelector(".scroll-btn").addEventListener("click", () => {
       document.querySelector("html").style.scrollBehavior = "unset";
     }, 1000);
   });
+
+const links = document.querySelectorAll(".menu-item a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+ 
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+document.querySelector(".mainTitle a").addEventListener("click", clickHandler);
